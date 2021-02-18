36ad0963dd28bf81863197c7c1ad92269df3c693
Glauber Faria <glauber.de.souza.faria@gmail.com>
2021-02-18 18:06:48 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Course from './components/Course';
+
+
+class App extends Component {
+
+
+  constructor(props){
+    super(props);
+    this.state={
+      courses: [
+        {
+          id:1,
+          name:'React',
+          category:'JavaScript',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+        },
+        {
+          id:2,
+          name:'Node',
+          category:'JavaScript',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+        },
+        {
+          id:3,
+          name:'PHP',
+          category:'PHP',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+        },
+        {
+          id:4,
+          name:'Laravel',
+          category:'PHP',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+        },
+        {
+          id:5,
+          name:'React',
+          category:'JavaScript',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+        },
+        {
+          id:6,
+          name:'Node',
+          category:'JavaScript',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+        },
+        {
+          id:7,
+          name:'PHP',
+          category:'PHP',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+        },
+        {
+          id:8,
+          name:'Laravel',
+          category:'PHP',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+        },
+        {
+          id:9,
+          name:'Laravel',
+          category:'PHP',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+        },
+      ]
+    }
+    this.remove = this.remove.bind(this);
+  }
+
+  remove(courseId){
+    const {courses} = this.state,
+      courseIndex = courses.findIndex(course => course.id == courseId);
+    courses.splice(courseIndex, 1);
+    this.setState({courses});
+  }
+  render(){
+    const {state} = this;
+    return(
+      <div className="App">
+        <h1>List of courses</h1>
+        <ul className="courses-list">
+          {
+            state.courses.map(course => <Course course={course} onRemove={this.remove}/>)
+          }
+        </ul>
+
+      </div>
+    );
+
+  }
 }
 
 export default App;
